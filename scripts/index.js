2bc1a1e6671bcb330276b34847bf8df443c2903f
dsrlg <sri.duvvuri10@gmail.com>
2024-07-22 22:45:57 -0700

    Final

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,76 @@
-const initialCards =[ 
-    {
+const initialCards = [
+  {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-},
-{
-    name:"Lake Louise",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-},
-{
-    name:"Bald Mountains",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-},
-{
-    name:"Latemar",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-},
-{
-    name:"Vanoise National Park",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-},
-{
-    name:"Lago di Braies",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-},
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
 const profileEditButton = document.querySelector("#profile-edit-button");
-const profileEditModal=document.querySelector('#profile-edit-modal');
-profileEditButton.addEventListener('click', () =>{
-    profileEditModal.classList.add('.modal_opened');
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profilename = document.querySelector(".profile__title");
+const profiledescription = document.querySelector(".profile__description");
+const profileInputTilte = document.querySelector("#profile-title-input");
+const profileInputDescription = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getcardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  return cardElement;
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileInputTilte.value = profilename.textContent;
+  profileInputDescription.value = profiledescription.textContent;
+  profileEditModal.classList.add("modal_opened");
 });
 
-const profileCloseButton = document.querySelector('#profile-modal-close');
-profileEditButton.addEventListener('click', () =>{
-    profileEditModal.classList.remove('.modal_opened');
+const profileCloseButton = document.querySelector("#profile-modal-close");
+profileCloseButton.addEventListener("click", () => {
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profilename.textContent = profileInputTilte.value;
+  profiledescription.textContent = profileInputDescription.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getcardElement(cardData);
+  cardListEl.append(cardElement);
 });
